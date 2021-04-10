89b8534f1edfea99a92e47e0fa742966ffc88b67
xersos <jacobcorentin@yahoo.fr>
2021-04-10 04:01:40 +0200

    feat: add loader on full screen

--- JCWebApp/src/app/app.component.ts
+++ JCWebApp/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '@env/environment';
+import { EnvironmentConfigService } from '@shared/services';
 
 @Component({
   selector: 'jc-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  isTestEnv = false;
   title = 'JCWebApp';
+  version = environment.version;
+
+  constructor(
+    private _environmentConfigService: EnvironmentConfigService
+  ) {}
+
+  ngOnInit() {
+    this.isTestEnv = !this._environmentConfigService.config.isProd;
+  }
 }
